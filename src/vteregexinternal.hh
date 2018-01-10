5037d1d4f0e14c6ec64615b5f9617420d79dc089
Adam Saponara <as@php.net>
2018-01-10 13:40:29 -0500

    libvte-2.91-0 (0.48.4-0ubuntu1)

---
+++ src/vteregexinternal.hh
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2015 Christian Persch
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+enum class VteRegexPurpose {
+        match,
+        search
+};
+
+gboolean _vte_regex_has_purpose(VteRegex *regex,
+                                VteRegexPurpose purpose);
+
+gboolean _vte_regex_get_jited(VteRegex *regex);
+
+#ifdef WITH_PCRE2
+const pcre2_code_8 *_vte_regex_get_pcre (VteRegex *regex);
+#endif
